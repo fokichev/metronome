e252aa7dc45e9825e4783e2ccc74903431602d32
Polina <54813973+fokicheva@users.noreply.github.com>
2021-12-03 04:22:47 +0000

    clean up

--- src/App.js
+++ src/App.js
@@ -27,21 +27,23 @@ class App extends React.Component {
   };
 
 
-// explanation for arrow notation instead of using bind: https://stackoverflow.com/questions/35287949/react-with-es7-uncaught-typeerror-cannot-read-property-state-of-undefined/35287996
+  // explanation for arrow notation instead of using bind: https://stackoverflow.com/questions/35287949/react-with-es7-uncaught-typeerror-cannot-read-property-state-of-undefined/35287996
+  // BPM slider onChange handler
   changeBpm = event => {
     const bpm = event.target.value;
     this.setState({
       bpm: bpm,
       currentBeat: 0,
-    }, this.resetPlay);
+    }, this.resetPlay); // resetPlay happens only after state is updates
   }
 
+  // time signature select onChange handler
   selectChange = event => {
     this.setState({perMeasure: event.target.value});
     this.resetPlay();
   }
 
-
+  // onClick play/pause button handler
   playPause(){
     const {active, bpm} = this.state;
     // pause
@@ -64,14 +66,15 @@ class App extends React.Component {
         this.playClick();
       }, ((60/bpm)*1000));
     }
-
   }
 
+  // play appropriate click sound and display visual representation through star symbols
   playClick(){
     const {perMeasure, currentBeat, measure} = this.state;
-    // if 1st beat of measure, play click1, otherwise click 2
     let temp = "";
+    // if 1st beat of measure, play click 1, otherwise click 2
     if (currentBeat % perMeasure === 0) {
+      // have to create new Audio object every time, otherwise JS will wait for previous click to finish to play new one
       const click1 = new Audio(click_1);
       click1.play();
       // if two measures displayed, reset measures
@@ -85,7 +88,7 @@ class App extends React.Component {
     else{
       const click2 = new Audio(click_2);
       click2.play();
-      // if perMeasure is 0, limit to 20 characters
+      // if perMeasure is 0 (no accent click), limit to 10 beats
       if (perMeasure == 0 && measure.length > 18){
         temp = "☆ ";
       }
@@ -94,45 +97,50 @@ class App extends React.Component {
       }
     }
     this.setState({
+      // increment stars
       measure: temp,
       // increment beat
       currentBeat: currentBeat + 1,
     });
   }
 
-
+  // determine BPM from user clicking the button in rhythm
   setBpm(){
-    // play sound
+    // play sound when button is pressed
     this.playClick();
     // get time
     const date = new Date();
     const time = date.getTime();
     // append time to times array
     const {times, perMeasure} = this.state;
-    // get last N number of elements where N is time signature
     let newTimes = times;
     newTimes.push(time);
-    newTimes = newTimes.slice((perMeasure*2) * -1);
+    // get last N number of elements where N is time signature (so user can adjust & average isn't skewed)
+    newTimes = newTimes.slice((perMeasure * 2) * -1);
     this.setState({
+      // calculating status for colour of BPM value ot let user know when its changeable
       calculating: true,
       times: newTimes,
     });
     this.calculateBPM();
+    // after 2 seconds, stop the calculating process & reset variables
     setTimeout(() => this.checkTimeout(time), 2000);
   }
 
+  // check if it's been 2 seconds since user last clicked "setBPM" button
   checkTimeout(time){
     const {active, times} = this.state;
     let lastTime = times.at(-1);
     if(time === lastTime){
       // 2 seconds passed = time out
-      // change color of BPM back to black
-      // reset times array
       this.setState({
+        // allows to change color of BPM back to black
         calculating: false,
+        // reset times array
         times: [],
       });
-      // if active, don't need to reset beat and measure
+      // if active, don't need to reset beat and measure after 2 seconds (as playPause resets locally)
+      // else, reset current beat
       if(!active){
         this.setState({
           currentBeat: 0,
@@ -143,41 +151,44 @@ class App extends React.Component {
     }
   }
 
-    calculateBPM(){
-      const {times} = this.state;
-      if (times.length > 1){
-        let sumOfGaps = 0;
-        for (let i = 1; i < times.length; i++){
-          sumOfGaps = sumOfGaps + (times[i] - times[i-1]);
-        }
-        let average = sumOfGaps/(times.length-1)
-        let bpm = Math.round((1000/average)*60);
-        if(bpm > 250){bpm = 250;}
-        if(bpm < 35){bpm = 35;}
-        this.setState({bpm: bpm});
+  // calculate BPM from times values
+  calculateBPM(){
+    const {times} = this.state;
+    if (times.length > 1){
+      let sumOfGaps = 0;
+      for (let i = 1; i < times.length; i++){
+        sumOfGaps = sumOfGaps + (times[i] - times[i-1]);
       }
+      let average = sumOfGaps/(times.length-1)
+      let bpm = Math.round((1000/average)*60);
+      // don't go over min/max boundaries
+      if(bpm > 250){bpm = 250;}
+      if(bpm < 35){bpm = 35;}
+      this.setState({bpm: bpm});
     }
+  }
 
-    plusMinus(x){
-      const {bpm} = this.state;
-      let temp = parseInt(bpm) + x;
-      if(temp > 250){temp = 250;}
-      if(temp < 35){temp = 35;}
-      this.setState({bpm: temp});
-      this.resetPlay();
+  // plus/minus onClick event handler to adjust BPM
+  plusMinus(x){
+    const {bpm} = this.state;
+    let temp = parseInt(bpm) + x; //parseInt as bpm becomes string from slider
+    // don't go over min/max boundaries
+    if(temp > 250){temp = 250;}
+    if(temp < 35){temp = 35;}
+    this.setState({bpm: temp});
+    this.resetPlay();
+  }
+
+  resetPlay(){
+    const {active, bpm} = this.state;
+    if(active){
+      // stop current interval, start a new one
+      clearInterval(this.interval);
+      this.interval = setInterval(() => {
+        this.playClick();
+      }, ((60/bpm)*1000));
     }
-
-    resetPlay(){
-      const {active, bpm} = this.state;
-      if(active){
-        // stop current interval, start a new one
-        clearInterval(this.interval);
-        this.interval = setInterval(() => {
-          this.playClick();
-        }, ((60/bpm)*1000));
-      }
-    }
-
+  }
 
   render(){
     const {active, bpm, perMeasure, calculating, measure} = this.state;
@@ -216,7 +227,6 @@ class App extends React.Component {
       </div>
     );
   }
-
 }
 
 export default App;
